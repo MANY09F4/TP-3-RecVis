4de2e39376cded5f4d27965154a7c01537764856
antoine mirri <antoine.mirri@dauphine.eu>
2024-11-21 22:36:14 +0100

    Test convnext

--- data.py
+++ data.py
@@ -62,7 +62,7 @@ data_transforms_224_DA = transforms.Compose(
 
         # Prétraitement
         transforms.ToTensor(),                  # Convertir en tenseur
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalisation pour niveaux de gris
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),  # Normalisation pour niveaux de gris
     ]
 )
 
