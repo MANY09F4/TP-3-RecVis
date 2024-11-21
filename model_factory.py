5d9a061d60cbecc7b4be0a549d265a09dc1db650
antoine mirri <antoine.mirri@dauphine.eu>
2024-11-21 22:30:26 +0100

    Test convnext

--- model_factory.py
+++ model_factory.py
@@ -61,7 +61,7 @@ class ModelFactory:
 
         if self.model_name == "vit_base16":
             if self.test_mode:
-                return data_transforms_224
+                return data_transforms_224_DA
             return data_transforms_224
 
         if self.model_name == "convnext_base":
