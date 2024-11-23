039c9ecba0e93318e32a693603d1f2b3a8449653
antoine mirri <antoine.mirri@dauphine.eu>
2024-11-23 14:54:11 +0100

    Test convnext

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def main():
 
     #Data initialization and loading
     train_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(args.data + "/train_images", transform=data_transforms),
+        train_dataset,
         batch_size=args.batch_size,
         shuffle=True,
         num_workers=args.num_workers,
@@ -238,7 +238,7 @@ def main():
     # )
 
     val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(args.data + "/val_images", transform=data_transforms_val),
+        val_dataset,
         batch_size=args.batch_size,
         shuffle=False,
         num_workers=args.num_workers,
